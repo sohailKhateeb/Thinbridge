4e284ce99f0f93468afaf5b7f1e24033167281a7
sohailKhateeb <almighty.sk@gmail.com>
2021-05-03 10:03:23 +0530

    Thinkbridge assignment
    assignment webapi and mvc

---
+++ WebApiInMVC/Mvc/Controllers/ProductController.cs
@@ -0,0 +1,64 @@
+﻿using Mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index(string role="user")
+        {
+            IEnumerable<mvcProductModal> prodList;
+            HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Products").Result;
+            prodList = response.Content.ReadAsAsync<IEnumerable<mvcProductModal>>().Result;
+            ViewBag.role = role;
+            return View(prodList);
+            
+        }
+
+        public PartialViewResult AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+                return PartialView("~/Views/Product/AddProduct.cshtml", new mvcProductModal());
+            
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Products/" + id.ToString()).Result;
+                //return PartialView("~/Views/Product/AddProduct.cshtml.cshtml", new mvcProductModal());
+                return PartialView("~/Views/Product/AddProduct.cshtml", response.Content.ReadAsAsync<mvcProductModal>().Result);
+            }
+        }
+
+      
+
+        [HttpPost]
+        public ActionResult AddOrEdit(mvcProductModal prd)
+        {
+            if (prd.ID == 0)
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PostAsJsonAsync("Products", prd).Result;
+                TempData["SuccessMessage"] = "Saved Successfully";
+            }
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PutAsJsonAsync("Products/" + prd.ID, prd).Result;
+                TempData["SuccessMessage"] = "Updated Successfully";
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            HttpResponseMessage response = GlobalVariables.WebApiClient.DeleteAsync("Products/" + id.ToString()).Result;
+            TempData["SuccessMessage"] = "Deleted Successfully";
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ WebApiInMVC/Mvc/Models/mvcProductModal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Mvc.Models
+{
+    public class mvcProductModal
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "This Field is Required")]
+        public string PrductName { get; set; }
+
+        [Required(ErrorMessage = "This Field is Required")]
+        public string ProductDesc { get; set; }
+
+        [Required(ErrorMessage = "This Field is Required")]
+        public Nullable<double> Rate { get; set; }
+
+
+        public Nullable<int> Quantity { get; set; }
+
+        public Nullable<int> MinQuantity { get; set; }
+    }
+}
